28dc9cf9638b9714c82ec05f5501e95038850eb2
Archer <duhb3ast@gmail.com>
2024-10-13 02:45:45 -0400

    updated logic in app file

--- app/App.php
+++ app/App.php
@@ -2,7 +2,8 @@
 
 declare(strict_types = 1);
 
-$dir = dirname(__DIR__) . DIRECTORY_SEPARATOR . "transaction_files";
+$dir = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+$fileDirectory = $dir . "transaction_files" . DIRECTORY_SEPARATOR;
 
 
 function returnDirFiles(string $directory): array{
@@ -11,10 +12,33 @@ function returnDirFiles(string $directory): array{
     return $files;
 }
 
+function parseFiles(array $files, string $directory){
+    #runs through each file in the specified directory assuming it is a csv file
+    foreach($files as $file){
+        #check if the file is not empty
+        $readableFile = (bool) filesize($directory . $file);
+        if(!$readableFile){
+            echo $file . " is empty, continuing... <br/>";
+            continue;
+        }
 
-print_r(returnDirFiles($dir));
+        echo "opening " . $directory . $file . "... <br/>";
+        $filePointer = fopen($directory . $file, "r");
+        #headers of the csv file and array to return
+        $headers = fgetcsv($filePointer);
+        $returnArray = [];
 
+        #loops through until eof and combines the headers with the corresponding data
+        while(($line = fgetcsv($filePointer)) != false){
+            $data = array_combine($headers, $line);
+            $returnArray[] = $data;
+        }
+        fclose($filePointer);
+        return $returnArray;
+    }
+}
 
-
-#hello
-#rgnegjneroeioln
+$files = returnDirFiles($fileDirectory);
+echo '<pre>';
+print_r(parseFiles($files, $fileDirectory));
+echo '</pre>';
